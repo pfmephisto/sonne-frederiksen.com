4b8d1e0282a480bd6d1b8070beff0c98c19e6fab
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2025-06-23 20:16:35 +0200

    Update Template
    Signed-off-by: mephisto <22501790+pfmephisto@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Hugo Dev Flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [go tailwindcss nodejs_24];
+        packages = with pkgs; [hugo];
+      };
+    });
+}
