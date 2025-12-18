595d4eeb35547b5c4ac94b6f128b319eaed07dd4
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2025-12-18 10:41:50 +0100

    Flake: Update & Add GitHub Copilot CLI
    Signed-off-by: Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -4,17 +4,35 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [go tailwindcss nodejs_24];
-        packages = with pkgs; [hugo];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            tailwindcss
+            nodejs_24
+          ];
+          packages = with pkgs; [
+            hugo
+            github-copilot-cli
+          ];
+        };
+      }
+    );
 }
